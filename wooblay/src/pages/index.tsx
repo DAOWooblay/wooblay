99e42dc06ee46a3dcabf9d3de7ae3a6c3c09b121
RealWooblay <jackaustinc66@gmail.com>
2024-05-05 09:29:39 +1000

    Final Final

--- wooblay/src/pages/index.tsx
+++ wooblay/src/pages/index.tsx
@@ -1,10 +1,9 @@
 import type { NextPage } from 'next';
+import Explorer from './explorer';
 
 const Home: NextPage = () => {
   return (
-    <div className=' text-3xl'>
-      <h1 className='text-red-500'>hi</h1>
-    </div>
+    <Explorer />
   );
 };
 
